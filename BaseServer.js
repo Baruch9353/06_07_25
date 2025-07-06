cc7794aa6926ff0e9f4047a0eaedaa999c1dbd1f
baruch9353 <baruchminzberg@gmail.com>
2025-07-06 17:30:16 +0300

    0607

--- BaseServer.js
+++ BaseServer.js
@@ -1,10 +1,34 @@
 import http from 'http';
 
-const server = http.createServer((req, res) =>{
-    
+// Exercise 1: Return a fixed text ============================
+
+// const server = http.createServer((req, res) =>{
+//     res.end('Hello from Node Server!')
+// });
+
+// server.listen(3000,()=>{
+//     console.log('Server running on port 3000');
+// });
+
+
+// Exercise 2: Return the request method ========================
+
+// const server = http.createServer((req, res) =>{
+//     res.end(`You used method: ${req.method}`)
+// });
+
+// server.listen(3000,()=>{
+//     console.log('Server running on port 3000');
+// });
+
+// Exercise 3: Return the request headers ===============================
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify(req.headers));
 });
 
-server.listen(()=>{
-    console.log('Server running on port 3000');
-    
-})
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
+
--- Exercise.js
+++ Exercise.js
@@ -1,52 +1,52 @@
-// Exercise 1: Log Request Method, URL, and HTTP Version ==================
+// // Exercise 1: Log Request Method, URL, and HTTP Version ==================
 
-import http from 'http';
+// import http from 'http';
 
-const server = http.createServer((req, res) => {
-  console.log('Method:', req.method);
-  console.log('URL:', req.url);
-  console.log('HTTP Version:', req.httpVersion);
-  res.end('OK');
-});
+// const server = http.createServer((req, res) => {
+//   console.log('Method:', req.method);
+//   console.log('URL:', req.url);
+//   console.log('HTTP Version:', req.httpVersion);
+//   res.end('OK');
+// });
 
-server.listen(3000, () => {
-  console.log('Server running on port 3000');
-});
-
-
-// Exercise 2: Respond Based on Headers and Method =====================================
-
-import http from 'http';
-
-const server = http.createServer((req, res) => {
-  if (req.method === 'POST' && req.headers['content-type'] === 'application/json') {
-    res.end('JSON accepted');
-  } else {
-    res.end('Unsupported request');
-  }
-});
-
-server.listen(3000, () => {
-  console.log('Server running on port 3000');
-});
+// server.listen(3000, () => {
+//   console.log('Server running on port 3000');
+// });
 
 
-// Exercise 3: Manual Route Handling with Method Check ====================
+// // Exercise 2: Respond Based on Headers and Method =====================================
 
-import http from 'http';
+// import http from 'http';
 
-const server = http.createServer((req, res) => {
-    if(req.url === '/hello' && req.method === 'GET'){
-        res.end('Hello from GET');
-    }
-    else if(req.url === '/hello' && req.method === 'POST'){
-        res.end('Hello from POST');
-    }
-    else{
-         res.end('Not supported');
-    }
-})
+// const server = http.createServer((req, res) => {
+//   if (req.method === 'POST' && req.headers['content-type'] === 'application/json') {
+//     res.end('JSON accepted');
+//   } else {
+//     res.end('Unsupported request');
+//   }
+// });
 
-server.listen(3000, () => {
-    console.log('Server running on port 3000');
-})
+// server.listen(3000, () => {
+//   console.log('Server running on port 3000');
+// });
+
+
+// // Exercise 3: Manual Route Handling with Method Check ====================
+
+// import http from 'http';
+
+// const server = http.createServer((req, res) => {
+//     if(req.url === '/hello' && req.method === 'GET'){
+//         res.end('Hello from GET');
+//     }
+//     else if(req.url === '/hello' && req.method === 'POST'){
+//         res.end('Hello from POST');
+//     }
+//     else{
+//          res.end('Not supported');
+//     }
+// })
+
+// server.listen(3000, () => {
+//     console.log('Server running on port 3000');
+// })
